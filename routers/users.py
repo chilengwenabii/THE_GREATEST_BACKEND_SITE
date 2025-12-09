a42ef12a187975eedc4c2c3afb27ad691023c7ef
CHILENGWE <chilengwenabii@gmail.com>
2025-12-09 14:36:37 +0300

    GOD HELP US

--- routers/users.py
+++ routers/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from typing import Optional
 from database import get_db
 from models import User
 from auth import get_current_user, get_password_hash
